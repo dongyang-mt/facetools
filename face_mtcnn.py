90230f218792b55da75d901dde6515301c35f648
dong.yang <dong.yang@mthreads.com>
2022-09-27 07:15:26 +0000

    test face parsing success in CPU

---
+++ face_mtcnn.py
@@ -0,0 +1,9 @@
+from PIL import Image
+from align.detector import detect_faces
+from align.visualization_utils import show_results
+
+img = Image.open('imgs/single.jpg')  # modify the image path to yours
+bounding_boxes, landmarks = detect_faces(img)  # detect bboxes and landmarks for all faces in the image
+print(bounding_boxes)
+print(landmarks)
+show_results(img, bounding_boxes, landmarks)  # visualize the results
--- parsing/face_parsing.py
+++ parsing/face_parsing.py
@@ -54,12 +54,12 @@ def vis_parsing_maps(im, parsing_anno, stride=1, show=False, save_im=False, save
     # return vis_im
 
 
-def parsing(imgs, cp='checkpoint/face_parsing.pth'):
+def parsing(imgs, cp='checkpoint/face_parsing.pth', device="cpu"):
 
     n_classes = 19
     net = BiSeNet(n_classes=n_classes)
-    net.cuda()
-    net.load_state_dict(torch.load(cp))
+    net.to(device)
+    net.load_state_dict(torch.load(cp, map_location=device))
     net.eval()
 
     to_tensor = transforms.Compose([
@@ -73,7 +73,7 @@ def parsing(imgs, cp='checkpoint/face_parsing.pth'):
             image = imgs.resize((512, 512), Image.BILINEAR)
             img = to_tensor(image)
             img = torch.unsqueeze(img, 0)
-            img = img.cuda()
+            img = img.to(device)
             out = net(img)[0]
             parsing_maps = out.squeeze(0).cpu().numpy().argmax(0).astype('float32')
             parsing_maps = cv2.resize(parsing_maps, shape, interpolation=cv2.INTER_NEAREST)
@@ -86,7 +86,7 @@ def parsing(imgs, cp='checkpoint/face_parsing.pth'):
                 image = img.resize((512, 512), Image.BILINEAR)
                 img = to_tensor(image)
                 img = torch.unsqueeze(img, 0)
-                img = img.cuda()
+                img = img.to(device)
                 out = net(img)[0]
                 parsing_maps = out.squeeze(0).cpu().numpy().argmax(0).astype('float32')
                 parsing_maps = cv2.resize(parsing_maps, shape, interpolation=cv2.INTER_NEAREST)
